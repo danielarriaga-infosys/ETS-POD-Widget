9bd6bb69c2c90a75dec951ca964c3e6e3689bdfe
Daniel Arriaga <75109673+danielarriaga-infosys@users.noreply.github.com>
2021-03-10 13:28:09 -0600

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ var blurHandler = function() {
 };
 
 var onSuccess = function(data) {
-    if (data[data.length - 1].source != 'agent') {
+    if (data[data.length - 1].source != 'visitor') {
         var text = data[data.length - 1].text;
         if (text.contains("?") {
             // Typical action to be performed when the document is ready:
